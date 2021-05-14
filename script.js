005f47bbb49a46c60a94b5c579a6ed009e583067
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-05-14 07:26:01 -0700

    update

--- script.js
+++ script.js
@@ -28,11 +28,12 @@ function handleFormSubmit(event) {
 
   //   Nelly, if you are reading this, I was only able to work on this document this morning. I will continue to refine this webpage over the weekend.
 
-  dest_card.innerHTML = `<img src=${photo} class="card-img-top" alt=${destination}|${location}>
+  //
+  dest_card.innerHTML = `<img src=${photo} class="card-img-top" alt=${destinationName}|${destinationLocation}>
     <div class="dest_card-body">
-      <h5 class="dest_card-title">${destination}</h5>
-      <h6>${location}</h6>
-      <p class="dest_card-text">${description}</p>
+      <h5 class="dest_card-title">${destinationName}</h5>
+      <h6>${destinationLocation}</h6>
+      <p class="dest_card-text">${destinationDescription}</p>
       <a href="#" btn_type="edit_btn" class="btn btn-warning">Edit</a>
       <a href="#" btn_type="delete_btn" class="btn btn-danger">Delete</a>
     </div>
