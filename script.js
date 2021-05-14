46e579e2b90bb6a28d316aad370788ecc405aedb
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-05-14 05:35:53 -0700

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Listen to the form being submitted
-document.querySelector("myForm").addEventListener("submit", handleFormSubmit);
+document.querySelector("#myForm").addEventListener("submit", handleFormSubmit);
 
 // Function needs to handle myForm being submitted
 function handleFormSubmit(event) {
@@ -15,38 +15,81 @@ function handleFormSubmit(event) {
     photo =
       "https://images.unsplash.com/photo-1502301197179-65228ab57f78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=632&q=80";
   }
-  const destinationDescription = event.target.elements["desc"].value;
+  const destinationDescription = event.target.elements["description"].value;
 
   //resets form to allow new entry
   resetFormValues();
 
-  //   By creating a new p tag for destination* and adding it to div#list will allow you to make it look approachable
+  //  By creating a new p tag for destination* and adding it to div#list will allow you to make it look approachable
   const dest_container = document.getElementById("list");
   const dest_card = document.createElement("div");
   dest_card.classList.add("dest_card", "yourCard");
   dest_card.style.width = "18rem";
 
-  //  Grabs form_container values and creates the card
-  let destinationCard = createDestinationCard(
-    destinationName,
-    destinationLocation,
-    destinationPhoto,
-    destinationDescription
-  );
+  dest_card.innerHTML = `<img src=${photo} class="card-img-top" alt=${destination}|${location}>
+    <div class="card-body">
+      <h5 class="card-title">${destination}</h5>
+      <h6>${location}</h6>
+      <p class="card-text">${description}</p>
+      <a href="#" btn_type="edit_btn" class="btn btn-warning">Edit</a>
+      <a href="#" btn_type="delete_btn" class="btn btn-danger">Delete</a>
+    </div>
+  `;
 
-  let wish_container = document.querySelector("wish_container");
+  dest_container.appendChild(dest_card);
 
-  //Change list title if list is empty
-  if (wish_container.children.length === 0) {
-    document.querySelector("#title").innerHTML = "Wish List";
-  }
-
-  //   overwrite my form with  destinationCard
-  document.querySelector("wish_container").appendChild(destinationCard);
+  dest_card.addEventListener("click", handleClick);
 }
 
-function resetFormValues(form) {
-  for (let i = 0; i < form.length; i++) {
-    form.elements[i].value = "";
+function resetFormValues() {
+  document.getElementById("destination").value = "";
+  document.getElementById("location").value = "";
+  document.getElementById("photo").value = "";
+  document.getElementById("description").value = "";
+}
+
+function handleClick(e) {
+  const elt = e.target;
+
+  if (elt.getAttribute("btn_type") === "delete_btn") {
+    deleteCard(elt);
+  } else if (elt.getAttribute("btn_type") === "edit_btn") {
+    handleEdit(elt);
+  }
+}
+
+function deleteCard(btn) {
+  btn.parentElement.parentElement.remove();
+}
+
+function handleEdit(elt) {
+  const cardBody = elt.parentElement;
+
+  const oldDest = cardBody.children[0];
+  const oldLoc = cardBody.children[1];
+  const oldDesc = cardBody.children[2];
+
+  const card = cardBody.parentElement;
+  const oldPhoto = card.children[0];
+
+  const newDest = prompt("New Destination", oldDest.innerText);
+  const newLoc = prompt("New Location", oldLoc.innerText);
+  const newPhoto = prompt("New Photo", oldPhoto.getAttribute("src"));
+  const newDesc = prompt("New Description", oldDesc.innerText);
+
+  if (newDest !== "") {
+    oldDest.innerText = newDest;
+  }
+
+  if (newLoc !== "") {
+    oldLoc.innerText = newLoc;
+  }
+
+  if (newPhoto !== "") {
+    oldPhoto.setAttribute("src", newPhoto);
+  }
+
+  if (newDesc !== "") {
+    oldDesc.innerText = newDesc;
   }
 }
