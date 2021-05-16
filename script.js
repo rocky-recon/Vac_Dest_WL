73c6768067582f0fb1720ae84ee483ddfdf4af10
damianmercado32@gmail.com <61065466+rocky-recon@users.noreply.github.com>
2021-05-16 09:00:55 -0700

    update js and index

--- script.js
+++ script.js
@@ -5,43 +5,52 @@ document.querySelector("#myForm").addEventListener("submit", handleFormSubmit);
 function handleFormSubmit(event) {
   // Stops the form from refreshing the page
   event.preventDefault();
+  const form = event.target;
 
   // Grab the values from form_container and storing them in wish_container
-  const destinationName = event.target.elements["destination"].value;
-  const destinationLocation = event.target.elements["location"].value;
-  let destinationPhoto = event.target.elements["photo"].value;
+  const destinationName = form.destination.value;
+  const destinationLocation = form.location.value;
+  let destinationPhoto = form.photo.value === "";
   //   Need to have a default photo if user fails to provide one
+  // Get the picture from Unsplash API
+  // my code goes here
+
   if (destinationPhoto === "") {
     photo =
-      "https://images.unsplash.com/photo-1502301197179-65228ab57f78?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=632&q=80";
+      "https://images.unsplash.com/photo-1507608869274-d3177c8bb4c7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1950&q=80";
   }
-  const destinationDescription = event.target.elements["description"].value;
+  const destinationDescription = form.description.value;
 
   //resets form to allow new entry
   resetFormValues();
 
   //  By creating a new p tag for destination* and adding it to div#list will allow you to make it look approachable
-  const dest_container = document.getElementById("list");
-  const dest_card = document.createElement("div");
-  dest_card.classList.add("dest_card", "yourCard");
-  dest_card.style.width = "18rem";
+
+  // const dest_container = document.getElementById("list");
+
+  const card = document.createElement("div");
+  card.setAttribute("class", "dest_ard");
+  card.setAttribute("style", "width: 18rem");
 
   //   Nelly, if you are reading this, I was only able to work on this document this morning. I will continue to refine this webpage over the weekend.
 
   //
-  dest_card.innerHTML = `<img src=${photo} class="card-img-top" alt=${destinationName}|${destinationLocation}>
-    <div class="dest_card-body">
-      <h5 class="dest_card-title">${destinationName}</h5>
+  card.innerHTML = `<img src=${photo} class="card-img-top" alt=${destinationName}|${destinationLocation}>
+    <div class="card-body">
+      <h5 class="card-title">${destinationName}</h5>
       <h6>${destinationLocation}</h6>
-      <p class="dest_card-text">${destinationDescription}</p>
+      <p class="card-text">${destinationDescription}</p>
       <a href="#" btn_type="edit_btn" class="btn btn-warning">Edit</a>
       <a href="#" btn_type="delete_btn" class="btn btn-danger">Delete</a>
     </div>
   `;
 
-  dest_container.appendChild(dest_card);
+  document.getElementById("card_Container").appendChild(card);
 
-  dest_card.addEventListener("click", handleClick);
+  card.addEventListener("click", handleClick);
+
+  document.getElementById("title").innnterText = "Wishlist";
+  resetFormValues(event);
 }
 
 function resetFormValues() {
@@ -51,29 +60,29 @@ function resetFormValues() {
   document.getElementById("description").value = "";
 }
 
-function handleClick(e) {
-  const elt = e.target;
+function handleClick(hCEvent) {
+  const event = hCEvent.target;
 
-  if (elt.getAttribute("btn_type") === "delete_btn") {
-    deleteCard(elt);
-  } else if (elt.getAttribute("btn_type") === "edit_btn") {
-    handleEdit(elt);
+  if (event.getAttribute("btn_type") === "delete_btn") {
+    deleteCard(hCEvent);
+  } else if (event.getAttribute("btn_type") === "edit_btn") {
+    handleEdit(hCEvent);
   }
 }
 
-function deleteCard(btn) {
-  btn.parentElement.parentElement.remove();
+function deleteCard(dCEvent) {
+  const event = dCEvent.target;
+  event.parentElement.parentElement.remove();
 }
 
-function handleEdit(elt) {
-  const cardBody = elt.parentElement;
+function handleEdit(hEdit) {
+  const event = hEdit.target;
+  const cardBody = event.parentElement.children;
 
-  const oldDest = cardBody.children[0];
-  const oldLoc = cardBody.children[1];
-  const oldDesc = cardBody.children[2];
-
-  const card = cardBody.parentElement;
-  const oldPhoto = card.children[0];
+  const oldDest = cardBody[0];
+  const oldLoc = cardBody[1];
+  const oldDesc = cardBody[2];
+  const oldPhoto = event.parentElement.parentElement.children[0];
 
   const newDest = prompt("New Destination", oldDest.innerText);
   const newLoc = prompt("New Location", oldLoc.innerText);
@@ -88,7 +97,7 @@ function handleEdit(elt) {
     oldLoc.innerText = newLoc;
   }
 
-  if (newPhoto !== "") {
+  if (newPhoto !== "" && newPhoto !== null) {
     oldPhoto.setAttribute("src", newPhoto);
   }
 
